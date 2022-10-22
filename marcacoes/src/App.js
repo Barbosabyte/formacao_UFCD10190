882f400320c162b79c214fa9e27c818b6656937e
Barbosabyte <barbosabyte.com>
2022-10-22 12:42:37 +0100

    primeiro componente

--- marcacoes/src/App.js
+++ marcacoes/src/App.js
@@ -1,24 +1,49 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Media from "./Media";
 
 function App() {
+  const title = "Blog";
+  let nrPosts = 100;
+  const atencao = "Número de comentários";
+  const comentarios = ["1", "2", "3", "4", "cinco", "zé", "pedro", "silva"];
+  const comentarios2 = [
+    { id: 1, texto: "Comentario sete" },
+    { id: 2, texto: "Comentario sete" },
+    { id: 3, texto: "Comentario sete" },
+    { id: 4, texto: "Comentario sete" },
+    { id: 5, texto: "Comentario sete" },
+    { id: 6, texto: "Comentario sete" },
+    { id: 7, texto: "Comentario sete" },
+  ];
+  const show = true;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Marcações
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="meuEstilo">{title}</h1>
+      <p>Posts</p>
+      <h3>Número de Posts: {nrPosts}</h3>
+
+      {show ? <Media /> : "Sem dados"}
+      <h4>
+        {atencao.toUpperCase()}: {comentarios.length}
+      </h4>
+      <h5>
+        {atencao.toUpperCase()}: {comentarios.length}
+      </h5>
+      <ul>
+        {comentarios.map((comentario) => (
+          <li key={comentario}>{comentario}</li>
+        ))}
+      </ul>
+      <h5>
+        {atencao.toUpperCase()}: {comentarios.length}
+      </h5>
+      <ul>
+        {comentarios2.map((comentario) => (
+          <li key={comentario.id}>{comentario.texto}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
---
+++ marcacoes/src/Media.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Media() {
+  let nrPosts = 100;
+  let nrTotal = 25;
+  return (
+    <>
+      <h3>Média de Posts: {nrPosts / nrTotal}</h3>
+    </>
+  );
+}
+
+export default Media;
